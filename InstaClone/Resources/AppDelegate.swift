24db2268e43fe87598c15b5f35bea9b31eb6c8f8
Andrey Maslov <andrey.ma2001@gmail.com>
2021-10-07 19:17:02 +0700

    fix bug Firebase prod

--- InstaClone/Resources/AppDelegate.swift
+++ InstaClone/Resources/AppDelegate.swift
@@ -5,16 +5,18 @@
 //  Created by Андрей Маслов on 07.10.2021.
 //
 
+
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+            FirebaseApp.configure()
+            return true
     }
 
     // MARK: UISceneSession Lifecycle
